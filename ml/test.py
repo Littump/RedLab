4d875c2a085370d6ec0e03e71c0a59768707dffd
Yakimov Roman <85906769+Littump@users.noreply.github.com>
2024-05-25 23:55:48 +0300

    0

---
+++ ml/test.py
@@ -0,0 +1,18 @@
+## можно отправить в редис тестовые данные, чтобы посмотреть, как они туда залетают
+## python3 test.py
+
+import json
+
+from redis_settings import rds
+
+
+points = [(i, i) for i in range(10)]
+
+data = {
+    'tabel_id': '123',
+    'name': 'metric',
+    'points': [{'x': point[0], 'y': point[1]} for point in points],
+    'is_ready': False,
+}
+
+rds.set('metric', json.dumps(data))
